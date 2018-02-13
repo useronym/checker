81063c9dcddeb96340da9bf0a4e9200b5b5b04f1
Adam Krupicka <akr@mail.muni.cz>
2018-02-13 18:39:59 +0100

    Tying the knot

--- src/Parse/Model.hs
+++ src/Parse/Model.hs
@@ -1,19 +1,34 @@
-{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE UnicodeSyntax   #-}
 module Parse.Model where
 
-import           Control.Applicative      hiding ((<|>))
+import           Control.Applicative   hiding ((<|>))
+import           Data.ByteString       (readFile)
+import           Data.Eq.Unicode
 import           Data.Function.Unicode
-import           Data.ByteString          (readFile)
+import           Data.List             (find)
+import           Data.Maybe            (fromJust, isJust)
 import           Data.Yaml
-import           Prelude                  hiding (readFile)
+import           Prelude               hiding (readFile)
 import           Types
 
 
 loadModel ∷ FilePath → IO (Either ParseException Model)
 loadModel path = readFile path >>= return ∘ (fmap $ build ∘ validate) ∘ decodeEither'
 
+-- Tie it all up.
 build ∷ ParsedModel → Model
-build = undefined
+build ParsedModel{..} =
+  let res = map (buildState res) parsedStates in
+    Model res
+  where buildState model ParsedState{..} = let thisId = fromJust parsedId in State {
+            stateId = thisId
+          , stateInit = parsedInit
+          , stateNext = map (\st → fromJust $ find ((≡ st) ∘ getStateId) model) parsedNext
+          , statePrev = filter (isJust ∘ find ((≡ thisId) ∘ stateId) ∘ stateNext) model
+          }
 
+-- Ensure uniqueness of state ids, that state ids referenced exists,
+-- and move initialness of a state from the global list to specific states.
 validate ∷ ParsedModel → ParsedModel
 validate = id
