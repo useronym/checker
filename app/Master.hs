4a5be1c3f4be48032f6080633da4216be87e548c
Adam Krupicka <akr@mail.muni.cz>
2018-03-09 10:35:22 +0100

    Improve master

--- app/Master.hs
+++ app/Master.hs
@@ -9,6 +9,7 @@ import           Control.Monad.Trans.Class   (lift)
 import           Data.Function.Unicode
 import           List
 import           Monad.ModelCheck
+import           Parse.Model
 import           Slave
 import           Types
 
@@ -16,16 +17,17 @@ import           Types
 spawnMaster ∷ MasterConfig → [NodeId] → Process ()
 spawnMaster _ [] = error "No slave nodes appear to be running"
 spawnMaster MasterConfig{model = m@PolyModel{..}, form = ϕ} slaves = do
-  say $ "Discovered slave nodes: " ++ show slaves
+  say $ "Discovered slave nodes (" ++ show (length slaves) ++ "): " ++ show slaves
   pids ← distribute (spawnSlave m ϕ)
   self ← getSelfPid
   mapM (`send` (self:pids)) pids
-  awaitResult pids ϕ
+  res ← awaitResult (build m) ϕ
+  liftIO $ putStrLn $ show res
   return ()
     where distribute = let ss        = map polyId polyStates
                            workloads = partitionN (length slaves) ss
                        in forM (zip workloads slaves) ∘ uncurry
 
 
-awaitResult ∷ [ProcessId] → Form → Process ModelCheckState
-awaitResult pids ϕ = newModelCheckState pids >>= stateServer ϕ
+awaitResult ∷ Model → Form → Process [(State, Bool)]
+awaitResult m ϕ = newModelCheckState [] >>= stateServer ϕ >>= evalModelCheck (lookupAll m ϕ)
