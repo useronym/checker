a2d325f128e3a86f506eb792ef4494ba703ff073
Adam Krupicka <akr@mail.muni.cz>
2018-04-05 16:16:12 +0200

    Nicer result output

--- app/Master.hs
+++ app/Master.hs
@@ -5,6 +5,7 @@ import           Config
 import           Control.Distributed.Process
 import           Control.Monad               (forM)
 import           Data.Function.Unicode
+import           Data.List                   (intercalate)
 import           List
 import           Monad.ModelCheck
 import           Parse.Model
@@ -19,13 +20,20 @@ spawnMaster MasterConfig{model = m@ValidatedModel{..}, form = ϕ} slaves = do
   pids ← distribute (spawnSlave m ϕ)
   self ← getSelfPid
   mapM_ (`send` (self:pids)) pids
-  res ← awaitResult (build m) ϕ
-  liftIO $ putStrLn $ show res
+  res ← getResult (build m) ϕ
+  liftIO $ putStrLn res
   return ()
     where distribute = let ss        = map parsedId validatedStates
                            workloads = partitionN (length slaves) ss
                        in forM (zip workloads slaves) ∘ uncurry
 
+getResult ∷ Model → Form → Process String
+getResult m ϕ = awaitResult m ϕ >>= return ∘ (renderResult ϕ)
 
 awaitResult ∷ Model → Form → Process [(State, Bool)]
 awaitResult m ϕ = newModelCheckState [] >>= stateServer ϕ >>= evalIxStateT (lookupAll m ϕ)
+
+renderResult ∷ Form → [(State, Bool)] → String
+renderResult ϕ res = intercalate "\n" ["Formula: " ++ show ϕ, header, body]
+  where header = "All states satisfied: " ++ show (and (map snd res))
+        body   = unlines $ map (\(s, r) → (stateId s) ++ ": " ++ (show r)) res
