1c604519cca6eabe81fa2b01e25795d52bf38da8
Adam Krupicka <akr@mail.muni.cz>
2018-05-12 10:08:46 +0200

    Hopefully improve checking estimate

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -5,16 +5,16 @@ import           Types
 
 size ∷ Form → Int
 size x = case x of
-  Truth     → 1
-  Not ϕ     → 1 + size ϕ
-  And ϕ ψ   → 1 + (max (size ϕ) (size ψ))
-  Next ϕ    → 1 + size ϕ
+  Truth     → 0
+  Not ϕ     → 0 + size ϕ
+  And ϕ ψ   → 0 + (max (size ϕ) (size ψ))
+  Next ϕ    → 0 + size ϕ
   Until ϕ ψ → 1 + (max (size ϕ) (size ψ))
-  Nom _     → 1
-  Data _    → 1
-  Var _     → 1
-  At _ ϕ    → 1 + size ϕ
-  Bind _ ϕ  → 1 + size ϕ
+  Nom _     → 0
+  Data _    → 0
+  Var _     → 0
+  At _ ϕ    → 0 + size ϕ
+  Bind _ ϕ  → 0 + size ϕ
 
 tetrate ∷ Int → Int → Int
 tetrate _ 0 = 1
